9f1ebca017e879f0f3c3fe82721680d37bc54dff
Srivastava-samarth <srivastavasamarth94@gmail.com>
2026-08-08 13:30:10 +0530

    fix: added one-off for merchant and updated model

--- database/models/merchant.go
+++ database/models/merchant.go
@@ -7,12 +7,18 @@ import (
 )
 
 type Merchant struct {
-	ID                uuid.UUID `json:"id" gorm:"primaryKey"`
-	MerchantReference string    `json:"merchant_reference" gorm:"unique"`
-	MerchantName      string    `json:"merchant_name"`
-	Email             string    `json:"email"`
-	PhoneNumber       *string   `json:"phone_number"`
-	Status            string    `json:"status"`
-	CreatedAt         time.Time `json:"created_at"`
-	UpdatedAt         time.Time `json:"updated_at"`
+	ID                uuid.UUID  `json:"id" gorm:"primaryKey"`
+	MerchantReference string     `json:"merchant_reference" gorm:"unique"`
+	MerchantName      string     `json:"merchant_name"`
+	Email             string     `json:"email"`
+	PhoneNumber       *string    `json:"phone_number"`
+	Status            string     `json:"status"`
+	MerchantType      string     `json:"merchant_type" gorm:"not null"`
+	KYC               []byte     `json:"-" gorm:"type:jsonb"`
+	KYCDate           *time.Time `json:"kyc_date"`
+	ComplianceStatus  string     `json:"compliance_status" gorm:"default:pending;not null"`
+	ComplianceDate    *time.Time `json:"compliance_date"`
+	ComplianceReason  string     `json:"compliance_reason"`
+	CreatedAt         time.Time  `json:"created_at"`
+	UpdatedAt         time.Time  `json:"updated_at"`
 }
