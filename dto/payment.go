e5ece2b55523aaf225e5d3a9c038dcc617f65b6c
Srivastava-samarth <srivastavasamarth94@gmail.com>
2026-09-13 02:32:26 +0530

    fix: refactor payment and settlement

--- dto/payment.go
+++ dto/payment.go
@@ -6,8 +6,8 @@ import (
 )
 
 type CreatePaymentRequest struct {
-	ReceiverMerchantID uuid.UUID       `json:"receiver_merchant_id" gorm:"not null"`
-	Amount             decimal.Decimal `json:"amount" gorm:"not null"`
-	Currency           string         `json:"currency" gorm:"not null"`
+	ReceiverMerchantID uuid.UUID       `json:"receiver_merchant_id" binding:"required"`
+	Amount             decimal.Decimal `json:"amount" binding:"required"`
+	Currency           string         `json:"currency" binding:"required"`
 	Description        string         `json:"description"`
 }
